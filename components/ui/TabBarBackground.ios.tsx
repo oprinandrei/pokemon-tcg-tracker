fe6d87f7b259dd8b344a2b018ecffc561ba7be72
Oprin Andrei <oprin.andrei@gmail.com>
2025-05-30 21:46:58 +0300

    Add lint and code formatter

--- components/ui/TabBarBackground.ios.tsx
+++ components/ui/TabBarBackground.ios.tsx
@@ -3,17 +3,17 @@ import { BlurView } from 'expo-blur';
 import { StyleSheet } from 'react-native';
 
 export default function BlurTabBarBackground() {
-  return (
-    <BlurView
-      // System chrome material automatically adapts to the system's theme
-      // and matches the native tab bar appearance on iOS.
-      tint="systemChromeMaterial"
-      intensity={100}
-      style={StyleSheet.absoluteFill}
-    />
-  );
+	return (
+		<BlurView
+			// System chrome material automatically adapts to the system's theme
+			// and matches the native tab bar appearance on iOS.
+			tint="systemChromeMaterial"
+			intensity={100}
+			style={StyleSheet.absoluteFill}
+		/>
+	);
 }
 
 export function useBottomTabOverflow() {
-  return useBottomTabBarHeight();
+	return useBottomTabBarHeight();
 }
